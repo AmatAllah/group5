88a4736c29807d6855052393b710c3b1bc0c50bd
MrWeber94 <abdelkareemelsayed2014@gmail.com>
2021-08-08 20:56:18 +0700

    v8.8

---
+++ index.php
@@ -0,0 +1,235 @@
+<?php 
+
+  // # php code ... 
+
+/*
+   php 
+
+   js 
+
+   html
+*/
+
+//echo    print 
+
+
+// echo 'Welcome to nti';
+
+// ECHO 'Welcome to nti';
+
+// print('welcome to php ');
+
+
+
+//  $name = "Root";   // string
+//  $age  = 20;       // int
+
+//  $name = "ahmed";
+
+//     echo $NAME;
+
+//    var_dump($name);
+
+// var_dump($age);
+
+
+//   define("size","50 kg",true);
+
+//    //echo constant("size");
+//    echo SIZE;
+
+
+
+
+
+// $num1 = "10";
+
+// $num2 = 10;
+
+//echo $num1 + $num2;     // - * / % **
+
+
+// $result = $num1 + $num2;
+
+  // $num1 += $num2;      >>>  $num1 = $num1 + $num2;
+  
+
+        // $num1  ==  $num2     value 
+        // $num1  ===  $num2    value && datatype
+        //   $num1 != $num2      value
+        //   $num1 !== $num2     value & datatype
+        
+        // if($num1 !== $num2){
+        //     echo '!=';
+        // }
+   
+
+    //     if($num1 <= $num2){
+
+
+    //   echo 'num1 < num2';
+
+    //     }
+
+
+    // $num1 = 10;
+
+
+    // echo $num1++.'<br>';   // 10   // post increment 
+
+    // echo $num1;      // 11
+
+
+    // echo ++$num1;    // pre increment
+
+
+    // $num1-1;
+
+     //echo   $num1--;       // post decrement
+     
+    //  echo --$num1;
+
+
+    // $num1 = 10;
+
+    // $num2 = 5;
+
+
+    // // and    && 
+    // cond1            cond2 
+    //  true              true    >>> true
+    //  false             true    >>> false
+    //  true              false   >>> false
+    //  false             false   >>> false
+
+    // // or    || 
+    // cond1               cond2
+    //  true                true    >>> true 
+    //  true                false   >>> true
+    //  false               true    >>> true
+    //  false               false   >>> false 
+
+    //  // xor    xor 
+    //  cond1               cond2
+    //  true                 true    >>> false
+    //  true                 false   >>> true
+    //  false                true    >>> true 
+    //  false                false   >>> false 
+
+
+   //not >>> !
+    // !true >>>> false  
+
+
+//   $age = 20;
+//   $name = "Root";
+
+
+//      echo   '<h2>';
+
+//     echo   'Name :  <h1>'.$name.'</h1><br>'.'age = '.$age;   
+    
+//     echo '</h2>';
+
+    //  $status =    (100<50)?"passed":"faild";   //ternary 
+
+    //  echo $status;
+
+//    $LoginStatus = false;
+
+
+//     if($LoginStatus == true){
+//         // code 
+//        echo 'User Profile';
+        
+//     }else{
+      
+//         echo 'Login Page';
+   
+//     }
+
+
+
+//    $age = 1;
+
+//     redirect A
+
+//     redirect B 
+
+//     reditrect C 
+
+
+//      if($age >= 20){
+
+//         echo 'Redierct A';
+
+//      }elseif( ($age >= 10 ) && ($age < 20)   ){
+
+//         echo 'Redirect B';
+//      }else{
+
+//         echo 'Redirect C';
+//      }
+
+
+  $color = "";
+
+switch ($color) {
+    case 'red':
+        # code...
+        echo 'color = red';
+        break;
+    
+    case 'blue':
+        #code 
+        echo 'color = blue';
+        break;
+    
+    case 'green':
+        #code 
+        echo "color = green";
+        break;
+
+
+
+    default:
+        # code...
+        echo 'not defined';
+        break;
+}
+
+
+
+
+
+
+// $age = 50;
+
+// switch ($age) {
+//     case ($age > 30):
+//         # code...
+
+//         echo 'test';
+//         break;
+    
+//     default:
+//         # code...
+//         echo 'not defined';
+//         break;
+// }
+
+
+
+// if($color == "red"){
+
+// }elseif($color == "blue"){
+
+// }
+
+
+?>
+
+
+
+
+
